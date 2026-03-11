a99abfa7bc7a741b6aaa88d0d9d3daa8e60f0d4e
OjekPantai <elvien.purnawan01@gmail.com>
2026-03-11 15:25:15 +0700

    refactor: remove redirect function from next config

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'elviencode.vercel.app',
-          },
-        ],
-        destination: 'https://elvien.net/:path*',
-        permanent: true,
-      },
-    ];
   },
 };
 
