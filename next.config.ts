41bdb68fbab70456d66147138ea658bf139f6685
OjekPantai <elvien.purnawan01@gmail.com>
2026-03-14 01:57:45 +0700

    fix: Add Content-Security-Policy (CSP) with Cloudinary support

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: res.cloudinary.com; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
+          },
+          {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
